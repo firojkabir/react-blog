60b73ccc3b0ef2137c06b63e08dc4403136d4a8a
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-06 14:30:26 +0200

    Article list modular and show other articles link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import AboutPage from './pages/AboutPage';
-import ArticleList from './pages/ArticleList';
+import ArticleListPage from './pages/ArticleListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './NavBar';
 import './App.css';
@@ -16,7 +16,7 @@ class App extends Component {
 					<div id="page-body">
 						<Route path="/" component={Homepage} exact />
 						<Route path="/about" component={AboutPage} />
-						<Route path="/articles-list" component={ArticleList} />
+						<Route path="/articles-list" component={ArticleListPage} />
 						<Route path="/article/:name" component={ArticlePage} />
 					</div>
 				</div>
