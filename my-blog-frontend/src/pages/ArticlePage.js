ad7e1e90b8c359dc08a31a53938f4ff7039a838a
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-09 22:34:35 +0200

    Add react hook

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticleList from '../components/ArticleList';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
@@ -6,14 +6,21 @@ import NotFoundPage from './NotFoundPage';
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3});
+    })
     
     if (!article) return <NotFoundPage />
 
-        const otherArticles = articleContent.filter(article => article.name !== name);
+    const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
