b35a6105b85a45a77bc8047e04802d265e5ebf03
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-11 23:19:29 +0200

    Create comment option and add comment from frontend

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticleList from '../components/ArticleList';
 import CommentsList from '../components/CommentsList';
 import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
 
@@ -33,6 +34,7 @@ const ArticlePage = ({ match }) => {
                 <p key={key}>{paragraph}</p>
             ))}
             <CommentsList comments={articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
             <h3>Other Articles:</h3>
             <ArticleList articles={otherArticles} />
         </>
