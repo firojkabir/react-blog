9f07c719e8b403004825817de9dda1122e1fa671
FirozKabir65 <firoj.kabir007@gmail.com>
2020-05-12 18:05:10 +0200

    Prepare to release

---
+++ my-blog-backend/src/build/precache-manifest.87ebf322087a84cac141624b5c9a47cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b26994bdf1ce0b42b731b121b1405b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3756e3ce46c95619a9e5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3756e3ce46c95619a9e5",
+    "url": "/static/js/main.c0eff378.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
